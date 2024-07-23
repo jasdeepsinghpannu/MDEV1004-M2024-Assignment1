43be92b346effd864c5d0a32908486103924e14f
jasdeepsingh <200540018@student.georgianc.on.ca>
2024-07-22 21:33:22 -0400

    Complied js to ts

--- Server/Config/app.js
+++ Server/Config/app.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+// 
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
